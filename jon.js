4a78eb65a823767cb83600e855c78cc2c4f0b009
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2024-04-19 18:30:47 +0200

    vcbfth

--- jon.js
+++ jon.js
@@ -1,1 +1,10 @@
+// Отримати бургер-меню та контент меню
+const burgerMenu = document.querySelector(".burger-menu");
+const menuContent = document.querySelector(".menu-content");
 
+// Додати обробник події для кліку на бургер-меню
+burgerMenu.addEventListener("click", () => {
+  // Змінити відображення контенту меню
+  menuContent.style.display =
+    menuContent.style.display === "block" ? "none" : "block";
+});
